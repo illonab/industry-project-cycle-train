0e24d82dcd50815697e42b9893599667876a673d
Ilona <ilona.b.mail@gmail.com>
2022-07-29 14:45:51 +0100

    home page sryling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -17,8 +17,7 @@ function Home() {
             <div className="main__solo btn-trip">Solo Trip</div>
           </div>
         </div>
-
-        <div className="main__schedule">
+        <div className="main__plan-trip">
           <h2 className="main__subheading">Details</h2>
           <label className="main__control main__container-from">
             From
@@ -31,7 +30,7 @@ function Home() {
               <option value="kentish">Kentish Town</option>
               <option value="kings cross">Kings Cross</option>
             </select>
-            <i class="bi bi-arrow-left-right"></i>
+            <i className="bi bi-arrow-left-right main__icon"></i>
             <select name="stations" className="main__select">
               <option value="Home">Work</option>
               <option value="finchley">Finchley Central</option>
@@ -42,26 +41,27 @@ function Home() {
               <option value="kings cross">Kings Cross</option>
             </select>
           </label>
+
+          <label className="main__control">
+            Date:
+            <input
+              type="date"
+              className="main__date"
+              name="date"
+              value="dd/mm"
+            />
+          </label>
+          <label className="main__control">
+            Time:
+            <input
+              type="time"
+              className="main__time"
+              name="time"
+              min="09:00"
+              max="21:00"
+            />
+          </label>
         </div>
-        <label className="main__control">
-          Date:
-          <input
-            type="date"
-            className="main__date"
-            name="date"
-            value="2022-07-29"
-          />
-        </label>
-        <label className="main__control">
-          Time:
-          <input
-            type="time"
-            className="main__time"
-            name="time"
-            min="09:00"
-            max="21:00"
-          />
-        </label>
         <Link to={"/results"}>
           <input type="submit" value="Go" className="main__btn cta-btn" />
         </Link>
