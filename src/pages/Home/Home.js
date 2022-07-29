06c2ad97f0988f975b71d46ffdc2558cfac69a12
Ilona <ilona.b.mail@gmail.com>
2022-07-29 10:03:44 +0100

    create home page styles

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,60 @@
 import "./Home.scss";
+import avatar from "../../assets/images/avatar.jpg";
 function Home() {
-  return <></>;
+  return (
+    <main className="main">
+      <h1 className="main__title">Hi Siphie! Welcome Back</h1>
+      <img src={avatar} alt="" className="main__img" />
+      <div className="main__trip">
+        <div className="main__solo btn-trip">Solo Trip</div>
+        <div className="main__cycle btn-trip main__cycle--active">
+          Cycle Train
+        </div>
+      </div>
+      <label className="main__control">
+        From
+        <select name="stations" className="main__select">
+          <option value="Home">Home</option>
+          <option value="finchley">Finchley Central</option>
+          <option value="hampsted">Hampstead</option>
+          <option value="camden">Camden Town</option>
+          <option value="victoria">Victoria</option>
+          <option value="kentish">Kentish Town</option>
+          <option value="kings cross">Kings Cross</option>
+        </select>
+        <i class="bi bi-arrow-left-right"></i>
+        <select name="stations" className="main__select">
+          <option value="Home">Work</option>
+          <option value="finchley">Finchley Central</option>
+          <option value="hampsted">Hampstead</option>
+          <option value="camden">Camden Town</option>
+          <option value="victoria">Victoria</option>
+          <option value="kentish">Kentish Town</option>
+          <option value="kings cross">Kings Cross</option>
+        </select>
+      </label>
+      <label className="main__control">
+        Date:
+        <input
+          type="date"
+          className="main__date"
+          name="date"
+          value="2022-07-29"
+        />
+      </label>
+      <label className="main__control">
+        Time:
+        <input
+          type="time"
+          className="main__time"
+          name="time"
+          min="09:00"
+          max="21:00"
+        />
+      </label>
+      <input type="submit" value="Go" className="main__btn cta-btn" />
+    </main>
+  );
 }
 
 export default Home;
